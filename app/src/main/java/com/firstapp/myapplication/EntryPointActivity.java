a674498c7cd575cf0faf8332fb6c3cfa72f91c9d
Andrey Klyuchko <fluxoid@gmail.com>
2018-11-19 01:00:10 +0300

    Домашка 2: Игра напёрстки. Сохранять состояние игры при смене ориентации устройства (пересоздание Activity)

--- app/src/main/java/com/firstapp/myapplication/EntryPointActivity.java
+++ app/src/main/java/com/firstapp/myapplication/EntryPointActivity.java
@@ -5,29 +5,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import com.firstapp.myapplication.firstlesson.StartWithExplicitEventActivity;
+import com.firstapp.myapplication.secondhomework.SecondHomeworkGameActivity;
 
 /**
  * Стартовая Activity, точка входа в приложение откуда запускаются все остальные activity
  */
 public class EntryPointActivity extends Activity {
 
+    /**
+     * Урок 1: явный запуск активити
+     */
     private View firstLessonButton;
+    /**
+     * Домашка 2: запуск игры "Пятнашки"
+     */
+    private View secondHomeworkGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_point);
 
-        configureFirstLessonButton();
+        configureFirstHomeworkButton();
+        configureSecondHomeworkButton();
     }
 
-    private void configureFirstLessonButton() {
+    private void configureFirstHomeworkButton() {
         firstLessonButton = findViewById(R.id.first_lesson_button);
         firstLessonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(EntryPointActivity.this, StartWithExplicitEventActivity.class);
                 intent.putExtra(StartWithExplicitEventActivity.ATTRIBUTE, "Пыщ пыщ");
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureSecondHomeworkButton() {
+        secondHomeworkGameButton = findViewById(R.id.second_homework_game_button);
+        secondHomeworkGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EntryPointActivity.this, SecondHomeworkGameActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/firstapp/myapplication/secondhomework/SecondHomeworkGameActivity.java
@@ -0,0 +1,153 @@
+package com.firstapp.myapplication.secondhomework;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.firstapp.myapplication.R;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static android.net.http.SslCertificate.restoreState;
+
+/**
+ * Игра "Напёрстки"
+ */
+public class SecondHomeworkGameActivity extends Activity {
+
+    private static final String TAG = SecondHomeworkGameActivity.class.getCanonicalName();
+    public static final String OPENED_ATTR = "opened";
+    public static final String GAME_STATE_ATTR = "gameState";
+
+    // новая игра
+    private Button newGameButton;
+
+    private ClickHandler clickHandler = new ClickHandler();
+    // состояние игры
+    private boolean[] gameState = new boolean[3];
+    //
+    private ImageView[] imageViews = new ImageView[3];
+    private Random random = new Random();
+
+    // флаг того, что вскрыли стаканы (если true - не надо обрабатывать клики по ним)
+    private boolean opened;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_homework_game);
+
+        imageViews[0] = findViewById(R.id.second_homework_image1);
+        imageViews[0].setOnClickListener(clickHandler);
+
+        imageViews[1] = findViewById(R.id.second_homework_image2);
+        imageViews[1].setOnClickListener(clickHandler);
+
+        imageViews[2] = findViewById(R.id.second_homework_image3);
+        imageViews[2].setOnClickListener(clickHandler);
+
+        newGameButton = findViewById(R.id.button_new_game);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newGame();
+                repaintCurrentGameState();
+            }
+        });
+        if (savedInstanceState == null) {
+            newGame();
+        } else {
+            restoreGameState(savedInstanceState);
+        }
+        repaintCurrentGameState();
+    }
+
+    private void repaintCurrentGameState() {
+        // отрисовали текущее состояние
+        if (opened) {
+            for (int i = 0; i < imageViews.length; i++) {
+                imageViews[i].setImageResource(gameState[i] ? R.drawable.ic_water_glass_win : R.drawable.homework2_naperstok_opened_fail);
+            }
+        } else {
+            for (int i = 0; i < imageViews.length; i++) {
+                imageViews[i].setImageResource(R.drawable.homework2_naperstok);
+            }
+        }
+    }
+
+    /**
+     * Сохранение состояния игры
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(OPENED_ATTR, opened);
+        outState.putBooleanArray(GAME_STATE_ATTR, gameState);
+    }
+
+    /**
+     * Восстановление состояния
+     * @param savedInstanceState
+     */
+    private void restoreGameState(Bundle savedInstanceState) {
+        opened = savedInstanceState.getBoolean(OPENED_ATTR);
+        gameState = savedInstanceState.getBooleanArray(GAME_STATE_ATTR);
+        newGameButton.setVisibility(opened ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public void newGame() {
+        opened = false;
+        // генерируем стакан, под которым
+        final int winIndex = random.nextInt(2);
+        Log.d(TAG, "Выигрышный стакан: " + winIndex);
+        for (int i = 0; i < 3; i++) {
+            gameState[i] = (winIndex == i);
+        }
+        newGameButton.setVisibility(View.INVISIBLE);
+    }
+
+    private int getIndexByView(ImageView imageView) {
+        for (int i = 0; i < imageViews.length; i++) {
+            if (imageView == imageViews[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public class ClickHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (opened) {
+                Toast.makeText(SecondHomeworkGameActivity.this, "Стаканы открыты, начните новую игру!!", Toast.LENGTH_SHORT)
+                    .show();
+                return;
+            }
+            newGameButton.setVisibility(View.VISIBLE);
+            ImageView imageView = (ImageView) v;
+            for (int i = 0; i < imageViews.length; i++) {
+                if (gameState[i]) {
+                    imageViews[i].setImageResource(R.drawable.ic_water_glass_win);
+                } else {
+                    imageViews[i].setImageResource(R.drawable.homework2_naperstok_opened_fail);
+                }
+            }
+
+            if (gameState[getIndexByView(imageView)]) {
+                Toast.makeText(SecondHomeworkGameActivity.this, "Вы выиграли!!", Toast.LENGTH_SHORT)
+                    .show();
+            } else {
+                Toast.makeText(SecondHomeworkGameActivity.this, "Вы проиграли!!", Toast.LENGTH_SHORT)
+                    .show();
+            }
+            opened = true;
+        }
+    }
+}
